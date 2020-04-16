4679daae786a11c8d9171c41bd6511d5acb306df
rahuladitya303 <rahuladitya303@gmail.com>
2020-04-16 11:04:43 +0530

    changes

--- runserver.py
+++ runserver.py
@@ -3,7 +3,8 @@ This script runs the Library_Management_System application using a development s
 """
 
 from os import environ
-from Library_Management_System import app, db
+from Library_Management_System import app as application
+from Library_Management_System import db 
 
 if __name__ == '__main__':
     db.create_all()
@@ -12,4 +13,4 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, debug=True, threaded=True)
+    application.run(HOST, PORT, debug=True, threaded=True)
